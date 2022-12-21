46439c10dbdd4c18a80db30dc3f3b6fead77ac95
Aaron <stephenaaroneaves@gmail.com>
2022-12-20 21:44:29 -0500

    Empty objects and array made HTML gameboard made and styled

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
-alert('america');
+let gameBoard = {
+    //make into module
+    board: [null, null, null, null, null, null, null, null, null]
+}
+
+let displayController = {
+     //make into module   
+}
+
+let playerFactory = {
+    //construct players here
+}
+
+let game = {
+    //game data goes here
+}
