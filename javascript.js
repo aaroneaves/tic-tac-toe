d6323ec719f48523431f1875ce5abf1c06ad80a2
Aaron <stephenaaroneaves@gmail.com>
2022-12-20 22:03:12 -0500

    Mock gameboard is now setup   Mock entries put into the array   DOM is now manipulatable   Function created to populate Gameboard with info from array

--- javascript.js
+++ javascript.js
@@ -1,16 +1,47 @@
+let x = "x";
+let o = "o"
+
 let gameBoard = {
     //make into module
-    board: [null, null, null, null, null, null, null, null, null]
+    board: [null, x, x, x, o, o, o, x, o, x]
 }
+
+const one = document.querySelector('#one');
+const two = document.querySelector('#two');
+const three = document.querySelector('#three');
+const four = document.querySelector('#four');
+const five = document.querySelector('#five');
+const six = document.querySelector('#six');
+const seven = document.querySelector('#seven');
+const eight = document.querySelector('#eight');
+const nine = document.querySelector('#nine');
+
+function populateGameBoard () {
+    one.textContent = gameBoard.board[1];
+    two.textContent = gameBoard.board[2];
+    three.textContent = gameBoard.board[3];
+    four.textContent = gameBoard.board[4];
+    five.textContent = gameBoard.board[5];
+    six.textContent = gameBoard.board[6];
+    seven.textContent = gameBoard.board[7];
+    eight.textContent = gameBoard.board[8];
+    nine.textContent = gameBoard.board[9];
+
+}
+
+populateGameBoard();
+
 
 let displayController = {
      //make into module   
 }
 
 let playerFactory = {
+    //make into factory
     //construct players here
 }
 
 let game = {
+    //make into module
     //game data goes here
 }
