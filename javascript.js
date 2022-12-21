4893fb501d6f8083b33d611d5127711e186047e5
Aaron <stephenaaroneaves@gmail.com>
2022-12-21 00:02:42 -0500

    Several things   "Narrator div" added   Player can now mark a box   Computer can make a choice and mark a box

--- javascript.js
+++ javascript.js
@@ -1,12 +1,10 @@
-let x = "x";
-let o = "o"
+const x = "x";
+const o = "o";
 
 let gameBoard = {
     //make into module
-    board: [null, x, x, x, o, o, o, x, o, x]
+    board: ["leave this blank", null, null, null, null, null, null, null, null, null]
 }
-
-
 
 const displayController = (function() {
     //not entirely sure why these variables are globally scoped
@@ -38,8 +36,88 @@ displayController.populateGameBoard();
 
 let playerFactory = {
     //make into factory
-    //construct players here
+    //construct players here    
 }
+
+one.addEventListener('click', () => {
+    if (gameBoard.board[1]) {
+        return;
+    } else {
+        gameBoard.board[1] = x;
+        displayController.populateGameBoard();
+    }
+});
+
+two.addEventListener('click', () => {
+    gameBoard.board[2] = x;
+    displayController.populateGameBoard();
+});
+
+three.addEventListener('click', () => {
+    gameBoard.board[3] = x;
+    displayController.populateGameBoard();
+});
+
+four.addEventListener('click', () => {
+    gameBoard.board[4] = x;
+    displayController.populateGameBoard();
+});
+
+five.addEventListener('click', () => {
+    gameBoard.board[5] = x;
+    displayController.populateGameBoard();
+});
+
+six.addEventListener('click', () => {
+    gameBoard.board[6] = x;
+    displayController.populateGameBoard();
+});
+
+seven.addEventListener('click', () => {
+    gameBoard.board[7] = x;
+    displayController.populateGameBoard();
+});
+
+eight.addEventListener('click', () => {
+    gameBoard.board[8] = x;
+    displayController.populateGameBoard();
+});
+
+nine.addEventListener('click', () => {
+    gameBoard.board[9] = x;
+    displayController.populateGameBoard();
+});
+
+const narrator = document.querySelector('.narrator');
+narrator.textContent = "Player's turn!"
+
+let player;
+
+let computer = {
+    turn: false
+};
+
+
+const computerTakesTurn = function() {
+    let choice = null;
+    computer.turn = true;
+    function getRandomNumber() {
+        const min = Math.ceil(1);
+        const max = Math.floor(10);
+        choice = Math.floor(Math.random() * (max - min) + min);
+    };
+
+    while (computer.turn) {
+        getRandomNumber();
+        if (gameBoard.board[choice] == null) {
+            gameBoard.board[choice] = o;
+            displayController.populateGameBoard();
+            computer.turn = false;
+        } 
+    };
+};
+
+computerTakesTurn();
 
 let game = {
     //make into module
